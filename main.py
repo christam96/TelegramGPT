f9b3a5b1160b47d146d3eb3c850def172397410e
Chris Tam <chris-tam8@hotmail.com>
2023-09-19 14:54:25 -0700

    script cleanup

--- main.py
+++ main.py
@@ -6,21 +6,13 @@ from langchain.document_loaders import TelegramChatFileLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
-from langchain.document_transformers import (
-    LongContextReorder,
-)
-
-###
-# Retrievers
-###
 from langchain.chains import RetrievalQA
-from langchain.llms import OpenAI
-# from langchain.indexes import VectorstoreIndexCreator
 
+# Load OpenAI API key
 load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# Instantiate model
+# Instantiate LLM model
 llm = ChatOpenAI(openai_api_key=SECRET_KEY)
 
 # Load Telegram data
@@ -48,16 +40,3 @@ qa = RetrievalQA.from_chain_type(llm=llm,
 query = "What has been discussed about zkRoute?"
 print("Query:", query)
 print(qa.run(query))
-
-# # Get relevant documents ordered by relevance score
-# docs = retriever.get_relevant_documents(query)
-
-# # Reorder the documents: Less relevant document will be at the middle of the list and more
-# # relevant elements at beginning / end.
-# reordering = LongContextReorder()
-# reordered_docs = reordering.transform_documents(docs)
-
-# # Confirm that the 4 relevant documents are at beginning and end.
-# for result in reordered_docs:
-#     print('Result:', result)
-#     print('\n\n')
